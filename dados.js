08c54f69a1b161d29dc1455339d77b6dc7c8e4ca
rhommardev2023 <123831335+rhommardev2023@users.noreply.github.com>
2025-03-02 17:21:26 -0300

    atualização
    alterações e estilização

--- dados.js
+++ dados.js
@@ -5,7 +5,7 @@ let dados =[
         Titulo: 'Tribunal Regional do Trabalho da 12ª Região ',
         Descricao: 'TRT-12 (Tribunal Regional do Trabalho da 12ª Região) é uma instituição judicial especializada em resolver conflitos trabalhistas. Sua principal função é aplicar a legislação trabalhista e garantir os direitos de trabalhadores e empregadores na região que abrange, no caso, o estado de Santa Catarina.',
         Link: 'https://portal.trt12.jus.br/assedio-moral-e-sexual-como-denunciar',
-        tags:'trabalho trabalhista denuncia denuncias direito advogado ajuda migrante '
+        tags:'trabalho trabalhista denuncia denuncias  migrante '
     
     },
     
@@ -14,7 +14,7 @@ let dados =[
         Titulo: 'Serviço Jesuíta a Migrantes e Refugiados',
         Descricao:'Organização dedicada à promoção e proteção dos direitos de migrantes e refugiados. Oferece serviços de acolhimento, orientação jurídica, apoio psicossocial e promoção da cidadania.',
         Link:"https://sjmrbrasil.org/contato/" ,
-        tags: 'organização organizacao ong migrante migrantes ajuda acessoria refugiados'
+        tags: 'organização organizacao ong migrante migrantes ajuda acessoria refugiados '
     },
     
     {
